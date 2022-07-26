c442a936882c8b5467ba879883914538abb00835
Jaryodd Carter <jaryodd@gmail.com>
2022-07-25 19:15:11 -0600

    almost done this time

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Col, Container, Row, Image } from 'react-bootstrap'
-import coloring  from "../images/coloring.jpg"
+import { Col, Container, Row, Image,Card } from 'react-bootstrap'
+import coloring from "../images/coloring.jpg"
+import me_and_son from "../images/me and son.jpg"
 
 export default function portfolio() {
   return (
@@ -8,11 +9,55 @@ export default function portfolio() {
       <Container>
         <Row className="px-4 my-5">
           <Col sm={7}>
-            <Image src="{coloring}" fluid rounded />
+            <Image src={me_and_son} fluid rounded />
           </Col>
           <Col sm={5}>sm=4</Col>
         </Row>
       </Container>
+      <Row>
+        <Col>
+          <Card border="primary" style={{ width: '18rem' }}>
+            <Card.Header>Almost_Faim_ous</Card.Header>
+            <Card.Body>
+              <Card.Title>Link to project</Card.Title>
+              <Card.Text>
+                This is a music app that my classmates and I made
+                <Card.Link href="https://github.com/ruxpin86/Almost-FAIM-ous">Almost_Faim_ous</Card.Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card border="primary" style={{ width: '18rem' }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Primary Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+                <Card.Link href="#">Card Link</Card.Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card border="primary" style={{ width: '18rem' }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Primary Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+                <Card.Link href="#">Card Link</Card.Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </main>
   )
 }
